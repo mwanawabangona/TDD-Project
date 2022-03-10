88c98e166141e21e274c2ead2927274d10c06c61
mwanawabangona <ellisallday@gmail.com>
2022-03-10 06:35:28 -0800

    Add factorial Method

--- solver.rb
+++ solver.rb
@@ -14,4 +14,12 @@ class Solver
       num.to_s
     end
   end
+  
+  def factorial(num)
+        raise ArgumentError if num.negative?
+        return 1 if num.zero?
+    
+        num * factorial(num - 1)
+      end
+    
 end
