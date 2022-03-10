b5d38639c597cbb51eeaefa17a9d49214122456f
mwanawabangona <ellisallday@gmail.com>
2022-03-10 06:17:10 -0800

    Created test for factorial

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -34,5 +34,14 @@ describe Solver do
     it 'takes number turns it into a string' do
       expect(@solver.fizzbuz(7)).to eq '7'
     end
+    
+
+    context'check factorial methods' do
+    describe '#describe' do
+        it 'returns 1 when o is passed' do
+        expect(@solver.factorial(0)).to eq(1)
+        end
+    end
+    end
 
 end
