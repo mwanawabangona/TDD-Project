a220907ebc30cab11e62f9bfc25f5669391953e9
Meri <mgogi13@freeuni.edu.ge>
2022-03-10 20:05:37 +0400

    Add method for the test case when num is divisible by 3 and 5

--- solver.rb
+++ solver.rb
@@ -3,6 +3,12 @@ class Solver
     str.reverse
   end
 
+  def fizzbuz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    end
+  end
+
   def factorial(num)
     raise ArgumentError if num.negative?
     return 1 if num.zero?
